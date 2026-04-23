ac77a395d0292f1888e59c20096135ea8ce0eb99
carolbcx <carolbarbosadecastro@gmail.com>
2026-04-23 11:15:19 -0300

    fix(Join): correct socket connection URL and improve code formatting

--- 06-devChat/client/src/components/Join/Join.jsx
+++ 06-devChat/client/src/components/Join/Join.jsx
@@ -1,17 +1,31 @@
-import stlyle from "./Join.module.css"
+import { useRef } from "react";
+import stlyle from "./Join.module.css";
 
-import {Input, Button} from "@mui/material"
+import { Input, Button } from "@mui/material";
+import io from "socket.io-client";
 
 const Join = () => {
-    return (
-        <div>
-            <h2>Bem-vindo ao devChat</h2>
-            <Input inputRef={usernameRef} placeholder="Nome de usuário" />
-            <Button sx={{mt:2, mb:2}} variant="contained" onClick={() => handleSubmit()}> Entrar
+  const usernameRef = useRef();
+  const handleSubmit = async () => {
+    const username = usernameRef.current.value;
+    if (!username.trim()) return;
+    const socket = io.connect("http://localhos:3001");
+    socket.emit("set_username", username);
+  };
+  return (
+    <div>
+      <h2>Bem-vindo ao devChat</h2>
+      <Input inputRef={usernameRef} placeholder="Nome de usuário" />
+      <Button
+        sx={{ mt: 2, mb: 2 }}
+        variant="contained"
+        onClick={() => handleSubmit()}
+      >
+        {" "}
+        Entrar
+      </Button>
+    </div>
+  );
+};
 
-            </Button>
-        </div>
-    )
-}
-
-export default Join
+export default Join;
